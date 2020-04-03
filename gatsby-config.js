85bde12259c35bc29f6ad0042e5b74376e346c22
Slashgear <antoine395.caron@gmail.com>
2020-04-03 21:02:32 +0200

    GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-77930802-1`,
       },
     },
     `gatsby-plugin-feed`,
