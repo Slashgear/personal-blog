a7f9d6c9e8c9db0f31ade1dc4334e8757811524f
Slashgear <antoine395.caron@gmail.com>
2020-04-04 16:04:46 +0200

    change posts path

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const BlogIndex = ({ data, location }) => {
                   marginBottom: rhythm(1 / 4),
                 }}
               >
-                <Link style={{ boxShadow: `none` }} to={`posts/${node.fields.slug}`}>
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
                   {node.frontmatter.lang === 'en' ? '🇬🇧' : '🇫🇷' } {title}
                 </Link>
               </h3>
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -83,14 +83,14 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
         >
           <li>
             {previous && (
-              <Link to={`posts${previous.fields.slug}`} rel="prev">
+              <Link to={previous.fields.slug} rel="prev">
                 ← {previous.frontmatter.title}
               </Link>
             )}
           </li>
           <li>
             {next && (
-              <Link to={`posts${next.fields.slug}`} rel="next">
+              <Link to={next.fields.slug} rel="next">
                 {next.frontmatter.title} →
               </Link>
             )}
