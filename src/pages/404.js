ddf1432ed872791e1f2d0a3960d377b13899dd1f
Antoine CARON <antoine.caron@bedrockstreaming.com>
2020-04-20 11:13:42 +0200

    customize 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import notFound from '../assets/chou-blanc.gif'
+import { rhythm } from '../utils/typography'
 
 const NotFoundPage = () => (
-  <div>
+  <div style={{
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    maxWidth: rhythm(24),
+    padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+  }}>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <img alt="chou blanc" src={notFound}/>
   </div>
 )
 
