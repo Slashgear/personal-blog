bcd7f6c61c05c6c60bbefd9557f798cef60e4a85
Slashgear <antoine395.caron@gmail.com>
2020-04-26 19:30:03 +0200

    fix 404 style

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,24 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import notFound from '../assets/chou-blanc.gif'
 import { rhythm } from '../utils/typography'
 
 const NotFoundPage = () => (
-  <div style={{
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    maxWidth: rhythm(24),
-    padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-  }}>
+  <div
+    style={{
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      color: 'var(--textNormal)',
+      maxWidth: rhythm(24),
+      padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+    }}
+  >
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <img alt="chou blanc" src={notFound}/>
+    <p>
+      <Link to="/">Back to the website</Link>
+    </p>
+    <img alt="chou blanc" src={notFound} />
   </div>
 )
 
