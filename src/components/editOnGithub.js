5fff25dc3c711ca6b147f1b054623d1dd7ba242b
Slashgear <antoine395.caron@gmail.com>
2020-05-02 14:02:18 +0200

    add edit post link

---
+++ src/components/editOnGithub.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+import { graphql, useStaticQuery } from 'gatsby'
+
+const Wrapper = styled.div`
+  margin-bottom: 1rem;
+`
+
+export const EditOnGithub = ({ slug }) => {
+  const {
+    site: {
+      siteMetadata: { repository },
+    },
+  } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          repository
+        }
+      }
+    }
+  `)
+
+  return (
+    <Wrapper>
+      <a
+        href={`${repository}/edit/source/src/pages${slug}index.md`}
+        target="_blank"
+      >
+        Edit this page on Github
+      </a>
+    </Wrapper>
+  )
+}
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import get from 'lodash/get'
+import Img from 'gatsby-image'
 
 import Bio from '../components/bio'
 import { Layout } from '../components/layout'
@@ -8,104 +9,111 @@ import { rhythm, scale } from '../utils/typography'
 import { TableOfContents } from '../components/tableOfContents'
 import SEO from '../components/seo'
 import { AvailableLanguages } from '../components/availableLanguages'
-import Img from 'gatsby-image'
+import { EditOnGithub } from '../components/editOnGithub'
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = get(this.props, `data.config.frontmatter.title`)
-    const language = get(this.props, `data.config.frontmatter.language`)
-    const siteBio = get(this, 'props.data.config.html')
-    const siteDescription = post.excerpt
-    const { previous, next } = this.props.pageContext
+export default function BlogPostTemplate({
+  data,
+  pageContext: { previous, next, slug },
+  location,
+}) {
+  const post = data.markdownRemark
+  const siteTitle = get(data, `config.frontmatter.title`)
+  const language = get(data, `config.frontmatter.language`)
+  const siteBio = get(data, 'config.html')
+  const siteDescription = post.excerpt
 
-    return (
-      <Layout
-        location={this.props.location}
-        config={this.props.data.config}
+  return (
+    <Layout
+      location={location}
+      config={data.config}
+      translations={post.frontmatter.translations}
+    >
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+        lang={post.frontmatter.lang}
         translations={post.frontmatter.translations}
+        image={
+          post.frontmatter.hero
+            ? post.frontmatter.hero.childImageSharp.image
+            : null
+        }
+      />
+      <h1 style={{ color: 'var(--header)' }}>{post.frontmatter.title}</h1>
+      {post.headings.length > 1 && (
+        <TableOfContents tableOfContents={post.tableOfContents} />
+      )}
+      <p
+        style={{
+          ...scale(-1 / 5),
+          display: 'block',
+          marginBottom: rhythm(1),
+          marginTop: rhythm(-1),
+        }}
       >
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description || post.excerpt}
-          lang={post.frontmatter.lang}
-          translations={post.frontmatter.translations}
-          image={
-            post.frontmatter.hero
-              ? post.frontmatter.hero.childImageSharp.image
-              : null
-          }
+        {post.frontmatter.date}
+      </p>
+
+      <AvailableLanguages
+        language={language}
+        translations={post.frontmatter.translations}
+      />
+
+      {post.frontmatter.hero && (
+        <Img
+          fluid={post.frontmatter.hero.childImageSharp.fluid}
+          alt={post.frontmatter.title}
         />
-        <h1 style={{ color: 'var(--header)' }}>{post.frontmatter.title}</h1>
-        {post.headings.length > 1 && (
-          <TableOfContents tableOfContents={post.tableOfContents} />
-        )}
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: 'block',
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
+      )}
 
-        <AvailableLanguages
-          language={language}
-          translations={post.frontmatter.translations}
-        />
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
 
-        {post.frontmatter.hero && (
-          <Img
-            fluid={post.frontmatter.hero.childImageSharp.fluid}
-            alt={post.frontmatter.title}
-          />
-        )}
+      <EditOnGithub slug={slug} />
 
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <Bio>
-          <div dangerouslySetInnerHTML={{ __html: siteBio }} />
-        </Bio>
+      <hr
+        style={{
+          marginBottom: rhythm(1),
+        }}
+      />
+      <Bio>
+        <div dangerouslySetInnerHTML={{ __html: siteBio }} />
+      </Bio>
 
-        <ul
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            listStyle: 'none',
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </Layout>
-    )
-  }
+      <ul
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+          listStyle: 'none',
+          padding: 0,
+        }}
+      >
+        <li>
+          {previous && (
+            <Link to={previous.fields.slug} rel="prev">
+              ← {previous.frontmatter.title}
+            </Link>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title} →
+            </Link>
+          )}
+        </li>
+      </ul>
+    </Layout>
+  )
 }
-
-export default BlogPostTemplate
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!, $language: String!) {
+    site {
+      siteMetadata {
+        repository
+      }
+    }
     config: markdownRemark(
       frontmatter: { language: { eq: $language }, type: { eq: "language" } }
     ) {
