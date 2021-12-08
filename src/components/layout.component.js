3bf38979e62388a90c496fc37c8f3db4aefed5d4
Antoine <antoine395.caron@gmail.com>
2021-12-08 21:52:20 +0100

    implement better footer

--- src/components/layout.component.js
+++ src/components/layout.component.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Navbar } from './navbar.component'
 import { MaxWidthWrapper } from './maxWidthWrapper'
-import { Footer } from './footer/footer.component'
+import { Footer } from './footer.component'
 
 export const Layout = ({ children, lang }) => (
   <>
     <Navbar lang={lang} />
     <MaxWidthWrapper>{children}</MaxWidthWrapper>
-    <Footer />
+    <Footer lang={lang} />
   </>
 )
